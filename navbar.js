d943575f3a6316fb747b4480aa06fdc3d99e50ba
epixieme <kirstie_hayes@hotmail.com>
2020-02-05 20:39:41 +0000

    fixed javascript

--- navbar.js
+++ navbar.js
@@ -4,7 +4,9 @@
 const classToggle=()=> {
   const navs = document.querySelectorAll('.Navbar__Items')
   nav.forEach(nav => nav.classList.toggle('Navbar__ToggleShow')); 
+  document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
 }
 
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle); // on click it calls and runs the classtoggle function when the screen is a mobile
+// document.querySelector('.Navbar__Link-toggle')
+//   .addEventListener('click', classToggle); // on click it calls and runs the classtoggle function when the screen is a mobile
