1a56a4e5ab19aa803d050c917f6164d4f3650cb7
epixieme <kirstie_hayes@hotmail.com>
2020-02-05 20:53:29 +0000

    fixed javascript pt2

--- navbar.js
+++ navbar.js
@@ -1,12 +1,12 @@
 /// controls the burger menu toggle and expands the menu when clicked 
 
 
-const classToggle=()=> {
+const toggleBurgerMenu=()=> {
   const navs = document.querySelectorAll('.Navbar__Items')
-  nav.forEach(nav => nav.classList.toggle('Navbar__ToggleShow')); 
-  document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow')); 
+
 }
 
+const callToggleBurgerMenu= document.querySelector('.Navbar__Link-toggle').addEventListener('click', callToggleBurgerMenu);
 // document.querySelector('.Navbar__Link-toggle')
 //   .addEventListener('click', classToggle); // on click it calls and runs the classtoggle function when the screen is a mobile
