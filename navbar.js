451ef425d4e3fd9e8e2307094e2047d9279704a0
epixieme <kirstie_hayes@hotmail.com>
2020-02-05 20:36:54 +0000

    updated javascript to es6 and created images folder

--- navbar.js
+++ navbar.js
@@ -1,8 +1,10 @@
-function classToggle() {
+/// controls the burger menu toggle and expands the menu when clicked 
+
+
+const classToggle=()=> {
   const navs = document.querySelectorAll('.Navbar__Items')
-  
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  nav.forEach(nav => nav.classList.toggle('Navbar__ToggleShow')); 
 }
 
 document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
+  .addEventListener('click', classToggle); // on click it calls and runs the classtoggle function when the screen is a mobile
